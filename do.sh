5f49a3a2d8d94d22b1427ae9bda949b4e6dd76a5
mejgun <mejgun@gmail.com>
2023-05-03 13:45:20 +0300

    git tests

--- do.sh
+++ do.sh
@@ -1,8 +1,9 @@
 set -e
+git clone https://github.com/LineageOS/hudson
 cd hudson
 git stash
 git checkout master
 git log --pretty=format:'%H %cs' > ../commits.list
 cd -
-cat commits.list |xargs -n2 ./1.sh
+# cat commits.list |xargs -n2 ./1.sh
 
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,11 +1,30 @@
 {-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Lib
   ( someFunc,
   )
 where
 
+import Data.Time (UTCTime)
+import Data.Time.Clock.POSIX (posixSecondsToUTCTime)
 import Devices qualified
+import System.Directory qualified as D
+import System.Process qualified as P
 
 someFunc :: IO ()
-someFunc = Devices.read "devices.json" >>= print
+someFunc = do
+  D.setCurrentDirectory "hudson"
+  _ <- P.readProcess "git" ["checkout", "master"] ""
+  s <- P.readProcess "git" ["log", "--pretty=format:%H %ct"] ""
+  let l = reverse $ parseLogEntry <$> lines s
+  mapM_ (\e -> P.readProcess "git" ["checkout", fst e] "") l
+  _ <- P.readProcess "git" ["checkout", "master"] ""
+  Devices.read "updater/devices.json" >>= mapM_ print
+
+parseLogEntry :: String -> (String, UTCTime)
+parseLogEntry s = case words s of
+  [x1, x2] -> (x1, t)
+    where
+      t = posixSecondsToUTCTime $ fromIntegral (read x2 :: Int)
+  _ -> error $ "bad log format: " ++ s
