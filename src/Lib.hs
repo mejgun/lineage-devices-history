e303e604e6e572d8e51ff97db127d40580726e0c
mejgun <mejgun@gmail.com>
2023-05-04 22:45:57 +0300

    start testing increment print

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,8 +3,10 @@
 
 module Lib (Lib.start) where
 
-import Control.Monad (foldM)
+import Control.Monad (foldM, foldM_)
 import Data.HashMap.Strict qualified as HM
+import Data.List (sortOn)
+import Data.Time.Clock.POSIX (posixSecondsToUTCTime)
 import Git qualified
 import Los.BuildFile qualified
 import Los.BuildFile.Parser qualified
@@ -22,9 +24,11 @@ start = do
   (res1, res2) <- foldM handleCommit (HM.empty, HM.empty) l
   putStrLn ""
   _ <- P.readProcess "git" ["checkout", "master"] ""
+  let commits = sortOn (snd . fst) $ filter (not . null . snd) $ HM.toList res2
+  -- mapM_ print $ sortOn fst $ HM.toList res1
+  -- mapM_ print commits
+  foldM_ (printIncrement res1) (("", posixSecondsToUTCTime 1), []) commits
   putStrLn "done"
-  mapM_ print $ HM.toList res1
-  mapM_ print $ HM.toList res2
 
 handleCommit :: Acc -> Git.Commit -> IO Acc
 handleCommit (devs, commits) c@(cmt, _) = do
@@ -32,3 +36,12 @@ handleCommit (devs, commits) c@(cmt, _) = do
   newdevs <- Los.Devices.update devs
   ts <- Los.BuildFile.read
   pure (newdevs, HM.insert c ts commits)
+
+printIncrement ::
+  Los.Devices.DeviceMap ->
+  (Git.Commit, [Los.BuildFile.Parser.Target]) ->
+  (Git.Commit, [Los.BuildFile.Parser.Target]) ->
+  IO (Git.Commit, [Los.BuildFile.Parser.Target])
+printIncrement devices _prevCommit (newCommit@(_, d)) = do
+  let a = filter (\(Los.BuildFile.Parser.Target m _) -> not (HM.member m devices)) d
+   in mapM_ print a >> pure newCommit
