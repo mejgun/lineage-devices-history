ea5b2a65cd4e3f6d30c16aefeef04d3077f94187
mejgun <mejgun@gmail.com>
2023-05-15 23:09:17 +0300

    links test

--- src/Html/Action.hs
+++ src/Html/Action.hs
@@ -31,25 +31,31 @@ toHtml devices (Diff.Switched mdl brnch1 brnch2) = do
   modelToHtml mdl
 
 brnchsAdded :: [Types.Branch] -> H.Html
-brnchsAdded = branchsToTags "tag is-medium is-success"
+brnchsAdded = branchsToTags "is-success"
 
 brnchsRemoved :: [Types.Branch] -> H.Html
-brnchsRemoved = branchsToTags "tag is-medium is-danger"
+brnchsRemoved = branchsToTags "is-danger"
 
 brnchsSwitched :: [Types.Branch] -> H.Html
-brnchsSwitched = branchsToTags "tag is-medium is-info"
+brnchsSwitched = branchsToTags "is-info"
 
 branchsToTags :: T.Text -> [Types.Branch] -> H.Html
 branchsToTags t =
-  mapM_ H.toHtml
+  sequence_
     . List.intersperse (", " :: H.Html)
-    . map (\(Types.Branch b) -> H.span (H.toHtml b) H.! HA.class_ (H.textValue t))
+    . map (\(Types.Branch b) -> H.span (H.toHtml b) H.! HA.class_ cl)
+  where
+    cl = H.textValue $ T.intercalate " " ["tag", "is-medium", t]
 
 modelToHtml :: Types.Model -> H.Html
 modelToHtml (Types.Model mdl) = H.td $ H.toHtml mdl
 
 nameToHtml :: Types.DeviceMap -> Types.Model -> H.Html
-nameToHtml (Types.DeviceMap devices) mdl =
+nameToHtml (Types.DeviceMap devices) mdl@(Types.Model m) =
   case HM.lookup mdl devices of
-    Just (Types.OEM o, Types.Name n) -> H.td $ H.toHtml $ T.intercalate " " [o, n]
+    Just (Types.OEM o, Types.Name n) ->
+      H.td $ do
+        H.toHtml o
+        " "
+        H.a H.! HA.href (H.textValue (T.concat [m, ".html"])) $ H.toHtml n
     _ -> error "device not found, could not be"
