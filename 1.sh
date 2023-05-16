095dbf3e8a341fb1740c1b4dd533294096710dcf
mejgun <mejgun@gmail.com>
2023-05-16 12:48:10 +0300

    netlify deploy test

--- 1.sh
+++
@@ -1,7 +0,0 @@
-set -e
-cd hudson
-git checkout $1
-cd ..
-cp hudson/lineage-build-targets h/lineage-build-targets-$2
-
-
--- do.sh
+++ do.sh
@@ -1,9 +1,4 @@
 set -e
-git clone https://github.com/LineageOS/hudson
+#git clone https://github.com/LineageOS/hudson
 cd hudson
-git stash
-git checkout master
-git log --pretty=format:'%H %cs' > ../commits.list
-cd -
-# cat commits.list |xargs -n2 ./1.sh
-
+git pull
