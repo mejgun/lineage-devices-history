26974d5ace7d21cd9d0cd21afbab7ac497c2603f
mejgun <mejgun@gmail.com>
2023-08-22 16:37:58 +0300

    update

--- src/Html/Index.hs
+++ src/Html/Index.hs
@@ -12,20 +12,21 @@ import Text.Blaze.Html5.Attributes qualified as HA
 import Types qualified
 
 write :: Types.DeviceMap -> HM.HashMap Types.OEM [Types.Model] -> H.Html
-write (Types.DeviceMap devices) brands = Html.Header.put "index" $ do
-  H.table H.! HA.class_ "table is-bordered is-striped" $
-    H.tbody $ do
-      H.tr $ do
-        H.td Html.Link.all
-        H.td Html.Link.github
-      mapM_
-        ( \(oem, vs) ->
-            H.tr $
-              do
-                H.td $ Html.Link.brand oem
-                H.td $ d vs
-        )
-        (sortOn (\(Types.OEM k, _) -> k) (HM.toList brands))
+write (Types.DeviceMap devices) brands =
+  Html.Header.put "LineageOS Unofficial Changelog" $ do
+    H.table H.! HA.class_ "table is-bordered is-striped" $
+      H.tbody $ do
+        H.tr $ do
+          H.td Html.Link.all
+          H.td Html.Link.github
+        mapM_
+          ( \(oem, vs) ->
+              H.tr $
+                do
+                  H.td $ Html.Link.brand oem
+                  H.td $ d vs
+          )
+          (sortOn (\(Types.OEM k, _) -> k) (HM.toList brands))
   where
     d = m1 . s . m2
     m1 =
